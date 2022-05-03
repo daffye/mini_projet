17ad2c0eec2918dee71f54d46b8598a792869eb1
daffye <daphneprenot72@gmail.com>
2022-05-03 13:45:08 +0200

    vitesse constante

--- pi_regulator.c
+++ pi_regulator.c
@@ -4,7 +4,6 @@
 #include <usbcfg.h>
 #include <chprintf.h>
 
-
 #include <main.h>
 #include <motors.h>
 #include <pi_regulator.h>
@@ -57,7 +56,8 @@ static THD_FUNCTION(PiRegulator, arg) {
         
         //computes the speed to give to the motors
         //distance_cm is modified by the image processing thread
-        //speed = pi_regulator(get_distance_cm(), GOAL_DISTANCE); //a modifier enlever
+        //speed = pi_regulator(get_distance_cm(), GOAL_DISTANCE);
+        speed = 10;
         //computes a correction factor to let the robot rotate to be in front of the line
         speed_correction = (get_line_position() - (IMAGE_BUFFER_SIZE/2));
 
