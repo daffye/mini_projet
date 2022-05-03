1202c9d09a02d31513f8a16d92ba645a10a4dfae
daffye <daphneprenot72@gmail.com>
2022-05-03 13:46:30 +0200

    modifications csts globales

--- main.h
+++ main.h
@@ -17,9 +17,9 @@ extern "C" {
 #define ROTATION_THRESHOLD		10
 #define ROTATION_COEFF			2 
 #define PXTOCM					1570.0f //experimental value
-#define GOAL_DISTANCE 			10.0f
+#define GOAL_DISTANCE 			8.0f    //10 --> 8
 #define MAX_DISTANCE 			25.0f
-#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
+#define ERROR_THRESHOLD			0.3f	//[cm] because of the noise of the camera 0.1 -> 0.3
 #define KP						800.0f
 #define KI 						3.5f	//must not be zero
 #define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
