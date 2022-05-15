f70b3f85058dec1e5f8951e1061a42501e450e20
daffye <daphneprenot72@gmail.com>
2022-05-15 15:08:07 +0200

    deplacement thread piregulator dans piregulator.c

--- pi_regulator.c
+++ pi_regulator.c
@@ -3,39 +3,48 @@
 #include <math.h>
 #include <usbcfg.h>
 #include <chprintf.h>
-
 #include <main.h>
 #include <motors.h>
 #include <pi_regulator.h>
 #include <process_image.h>
+#include <mouvement.h>
 
-//simple PI regulator implementation
-int16_t pi_regulator(float distance, float goal){
 
-	float error = 0;
-	float speed = 0;
+static THD_WORKING_AREA(waPiRegulator, 256);
+static THD_FUNCTION(PiRegulator, arg) {
 
-	static float sum_error = 0;
+	chRegSetThreadName(__FUNCTION__);
+	(void)arg;
 
-	error = distance - goal;
+	int16_t speed = 0;
+	int16_t speed_correction = 0;
 
-	//disables the PI regulator if the error is to small
-	//this avoids to always move as we cannot exactly be where we want and 
-	//the camera is a bit noisy
-	if(fabs(error) < ERROR_THRESHOLD){
-		return 0;
+	go_forward();
+
+	chThdSleepMilliseconds(500);
+
+	while(1){
+		//computes the speed to give to the motors
+		//distance_cm is modified by the image processing thread
+		speed = SPEED;
+		//computes a correction factor to let the robot rotate to be in front of the line
+		speed_correction = (get_line_position() - (IMAGE_BUFFER_SIZE/2));
+
+		//if the line is nearly in front of the camera, don't rotate
+		if(abs(speed_correction) < ROTATION_THRESHOLD){
+			speed_correction = 0;
+		}
+
+		//applies the speed from the PI regulator and the correction for the rotation
+		if(get_intersection_bool() != 1){		//if there is no intersection then it continues following the black line
+		 	right_motor_set_speed(speed - ROTATION_COEFF * speed_correction);
+		    left_motor_set_speed(speed + ROTATION_COEFF * speed_correction);
+		    }
+
+		chThdSleepMilliseconds(100);
 	}
+}
 
-	sum_error += error;
-
-	//we set a maximum and a minimum for the sum to avoid an uncontrolled growth
-	if(sum_error > MAX_SUM_ERROR){
-		sum_error = MAX_SUM_ERROR;
-	}else if(sum_error < -MAX_SUM_ERROR){
-		sum_error = -MAX_SUM_ERROR;
-	}
-
-	speed = KP * error + KI * sum_error;
-
-    return (int16_t)speed;
+void pi_regulator_start(void){
+	chThdCreateStatic(waPiRegulator, sizeof(waPiRegulator), NORMALPRIO, PiRegulator, NULL);
 }
