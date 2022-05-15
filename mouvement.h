50af76d6b1ce74476d11dce2e2aed2bae6da8490
daffye <daphneprenot72@gmail.com>
2022-05-15 15:07:25 +0200

    dernier clean mouvement.h

--- mouvement.h
+++ mouvement.h
@@ -13,21 +13,25 @@
 #define DETECTION_DISTANCE          300    // Desired sensor distance detection, in delta
 #define FULL_PERIMETER_DEG          360.0f     //Degrees needed for the full perimeter
 #define NSTEP_ONE_TURN              1000    // number of step for 1 turn of the motor
-#define DISTANCE                	50      //mm
+#define DISTANCE                	50      //[mm]
 #define CONVERSION_CM_MM            10      // Decimal difference between cm and mm
 #define NSTEP_ONE_TURN              1000    // number of step for 1 turn of the motor
-#define WHEEL_PERIMETER             13      // cm
-#define CORRECTION					1.3
-#define CORRECTION_ANGLE			21		//degre°
+#define WHEEL_PERIMETER             13      //[cm]
+#define CORRECTION					1.3		//Correction factor to counter any parasite movements
+#define CORRECTION_ANGLE			20		//degre°
 #define SPEED					    500
-#define EPUCK_RADIUS                40      //mm
-#define PETITE_DIST					20
-#define MOY_DIST					24
-#define GRD_DIST					130
+#define BYPASS_CORRECTION           45      //[mm]
+#define PETITE_DIST					20		//[mm]
+#define MOY_DIST					24		//[mm]
+#define GRD_DIST					115		//[mm]
+#define DIST_INTERSECT				47		//[mm]
+#define DIST_TO_PATH				30		//[mm]
 #define TOF_MIN						85
 #define TOF_FAR						100
 
 void avoid_start(void);
 void pi_regulator_start(void);
+void intersection(void);
+void go_forward(void);
 
 #endif /* MOUVEMENTL_H */
